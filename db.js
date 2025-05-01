f317047fe666a6fdac26bc54cc0d7bf025b34124
Handikurniawan1 <handikurniawan59@gmail.com>
2025-05-01 12:29:26 +0700

    update db.js pakai env Railway

--- db.js
+++ db.js
@@ -1,12 +1,26 @@
+// import pkg from 'pg';
+// const { Pool } = pkg;
+
+// const pool = new Pool({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'manga_db',
+//   password: 'handi',
+//   port: 5432,
+// });
+
+// export default pool;
+
 import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'manga_db',
-  password: 'handi',
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: false }, // biar Railway aman SSL-nya
 });
 
 export default pool;
